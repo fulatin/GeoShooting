6171466a49a9016f6016bca4b2012d99c88adb24
jiahui <jiahui@DESKTOP-EILJPJF>
2025-07-09 00:31:05 +0800

    添加项目文件。

---
+++ GeoShooting/Constant.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+const int WINDOW_WIDTH = 1920; // 窗口宽度
+const int WINDOW_HEIGHT = 1080; // 窗口高度
---
+++ GeoShooting/GameObject.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include<graphics.h>
+
+class GameObject {
+public:
+	float x, y; // 物体的位置
+	float width, height; // 物体的宽度和高度
+	GameObject(float x, float y, float width, float height)
+		: x(x), y(y), width(width), height(height) {
+	}
+	virtual ~GameObject() = default;
+	// 绘制物体
+	virtual void draw() = 0;
+	virtual void update(ExMessage msg) = 0;
+};
